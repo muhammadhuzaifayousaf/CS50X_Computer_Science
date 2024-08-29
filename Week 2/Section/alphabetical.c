923061b7d98fada41505fbbb2fc8543b15e2acb4
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-08-29 12:55:33 +0500

    Adding Section' files of Week 2

---
+++ Week 2/Section/alphabetical.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string phrase = get_string("Enter a phrase: ");
+    int length = strlen(phrase);
+    for (int i = 0; i < length - 1; i++)
+    {
+        if (phrase[i] > phrase[i + 1])
+        {
+            printf("Not in alphabetical order!\n");
+            return 0;
+        }
+    }
+    printf("In alphabetical order!\n");
+    return 0;
+}
