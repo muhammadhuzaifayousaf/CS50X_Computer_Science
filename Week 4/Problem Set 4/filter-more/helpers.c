094b8d18cc546ba3046447f9d68528a2b0812e79
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-09-21 19:42:22 +0500

    Adding filter (more comfortable) Problem Set 4

---
+++ Week 4/Problem Set 4/filter-more/helpers.c
@@ -0,0 +1,151 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Calculate the average of the RGB values
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            // Set each color value to the average
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap the pixels
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a copy of the image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Apply the blur filter
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int redSum = 0, greenSum = 0, blueSum = 0;
+            int count = 0;
+
+            // Sum the values of the neighboring pixels
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    // Check if the neighboring pixel is within the image bounds
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        redSum += copy[ni][nj].rgbtRed;
+                        greenSum += copy[ni][nj].rgbtGreen;
+                        blueSum += copy[ni][nj].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            // Calculate the average values
+            image[i][j].rgbtRed = round(redSum / (float)count);
+            image[i][j].rgbtGreen = round(greenSum / (float)count);
+            image[i][j].rgbtBlue = round(blueSum / (float)count);
+        }
+    }
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a copy of the image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Define Sobel kernels
+    int Gx[3][3] = {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+
+    int Gy[3][3] = {
+        {-1, -2, -1},
+        {0, 0, 0},
+        {1, 2, 1}
+    };
+
+    // Apply Sobel operator
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int redX = 0, greenX = 0, blueX = 0;
+            int redY = 0, greenY = 0, blueY = 0;
+
+            // Sum the values of the neighboring pixels
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    // Check if the neighboring pixel is within the image bounds
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        redX += copy[ni][nj].rgbtRed * Gx[di + 1][dj + 1];
+                        greenX += copy[ni][nj].rgbtGreen * Gx[di + 1][dj + 1];
+                        blueX += copy[ni][nj].rgbtBlue * Gx[di + 1][dj + 1];
+
+                        redY += copy[ni][nj].rgbtRed * Gy[di + 1][dj + 1];
+                        greenY += copy[ni][nj].rgbtGreen * Gy[di + 1][dj + 1];
+                        blueY += copy[ni][nj].rgbtBlue * Gy[di + 1][dj + 1];
+                    }
+                }
+            }
+
+            // Calculate the final values
+            int red = round(sqrt(redX * redX + redY * redY));
+            int green = round(sqrt(greenX * greenX + greenY * greenY));
+            int blue = round(sqrt(blueX * blueX + blueY * blueY));
+
+            // Ensure the values are capped at 255
+            image[i][j].rgbtRed = red > 255 ? 255 : red;
+            image[i][j].rgbtGreen = green > 255 ? 255 : green;
+            image[i][j].rgbtBlue = blue > 255 ? 255 : blue;
+        }
+    }
+}
